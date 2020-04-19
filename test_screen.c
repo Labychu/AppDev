1eb5a0a10adbb8b482a08217592a766579d21e91
Labychu <56232252+Labychu@users.noreply.github.com>
2020-04-19 23:55:58 +0300

    sound 1st update

--- test_screen.c
+++ test_screen.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "screen.h"
+#include "comm.h"
+#include "sound.h"
 
 int main(void) {
-	printf("Press any key to continue\n");
-	getchar();	//wait for the user to press a key
-	
-	for (int i = 0; i < 15; i++) {
-		setColors(RED, BLK);
+	Position cur = getScreenSize();
+	char postdata[1000];
+	sprintf(postdata, "row=%d&col=%d&id=e1900315", cur.row, cur.col);
+	sendpost(URL, postdata);
+	gotoXY(1, 1);
+	printf("Screen size: row=%d, col=%d\n", cur.row, cur.col); 
+	printf("Today we will make some animations. Press any key to continue\n");
+	getchar();
+	getchar(); 
+	int ff, bb;
+	float step = (float)cur.col / cur.row;  
+	for (int i = 1; i <= cur.row; i++)
+	{
 		clrscr();
-		drawRect(i + 1, i * 3, 2, 4);
-		gotoXY(15 - i, i * 3);
-		setColors(GRN, BLK);
-		printf("Hello\n");
-		usleep(500000);
+		setFgColor(RED);
+		drawRect(i, i * 3, 2, 4);
+		setFgColor(GRN);  
+		gotoXY(cur.row - i, (i - 1) * step + 1);
+		printf("HELLO\n");
+		usleep(500000);  
 	}
 
-	for (int i = 0; i < 15; i++) {
-		setColors(RED, BLK);
-		clrscr();
-		drawRect(15 - i, i * 3 + 45, 2, 4);
-		gotoXY(i, i * 3 + 45);
-		setColors(GRN, BLK);
-		printf("Hello\n");
-		usleep(500000);
-	}
-
-	resetColor();
+	getchar(); 
+	resetColors();
 	clrscr();
-	printf("Press any key to continue\n");
-	getchar();  // Wait for user to press a key
-	printf("Default color\n");
+	printf("Color is set back to default\n");
+	getchar();
+	FILE *fptr = fopen("test.wav", "r");
+	WAVHeader header = readWAVHeader(fp);
+	fclose(fptr);
+	displayWAVHeader(header);  
 	return 0;
 }
