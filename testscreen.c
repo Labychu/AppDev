5baf22c5ad8a58c253e683a10d77c025f5127257
Labychu <e1900323@shell.vamk.fi>
2020-05-01 20:46:12 +0300

    Rename

---
+++ testscreen.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "screen.h"
+#include "comm.h"
+#include "sound.h"
+
+int main(void) {
+	Position cur = getScreenSize();
+	char postdata[1000];
+	sprintf(postdata, "row=%d&col=%d&id=e1900323", cur.row, cur.col);
+	sendpost(URL, postdata);
+	gotoXY(1, 1);
+	clrscr();
+	printf("Screen size: row=%d, col=%d\n", cur.row, cur.col); 
+	printf("Today we will make some animations. Press any key to continue\n");
+	getchar();
+	getchar();
+	/* 
+	int ff, bb;
+	float step = (float)cur.col / cur.row;  
+	
+	for (int i = 1; i <= cur.row; i++) {
+		clrscr();
+		setFgColor(RED);
+		drawRect(i, i * 3, 2, 4);
+		setFgColor(GRN);  
+		gotoXY(cur.row - i, (i - 1) * step + 1);
+		printf("HELLO\n");
+		usleep(500000);  
+	}
+	
+	getchar(); 
+	resetColor();
+	*/
+	clrscr();
+	printf("Color is set back to default\n");
+	getchar();
+	FILE *fptr = fopen("1.wav", "r");
+	WAVHeader header = readWAVHeader(fptr);
+	fclose(fptr);
+	displayWAVHeader(header);
+	sprintf(postdata, "", );
+	return 0;
+}
